632ba9df30236db5ca66f56ab9742d7d04fdfc23
AndyRosenberg <andynaterosenberg@gmail.com>
2018-10-08 00:18:54 -0700

    Production

--- db.py
+++ db.py
@@ -2,10 +2,8 @@ from app import *
 from urllib.parse import urlparse
 
 if 'HEROKU' in os.environ:
-    urlparse.uses_netloc.append('postgres')
     url = urlparse.urlparse(os.environ["DATABASE_URL"])
     pg_db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-    db_proxy.initialize(pg_db)
 else:
     pg_db = PostgresqlDatabase('portfolio_dev', user=os.environ['PG_USER'], password=os.environ['PG_PW'],
                                host='localhost', port=5432)
